850385f8758e86401c2ec2dcb8adb0e3d79cde42
Mykola Gurov <ngurov@gmail.com>
2017-11-27 22:45:18 +0100

    product service

--- src/main/java/com/github/mgurov/product/ProductConnector.java
+++ src/main/java/com/github/mgurov/product/ProductConnector.java
@@ -1,4 +1,9 @@
 package com.github.mgurov.product;
 
+import java.util.List;
+
 public class ProductConnector {
+    public List<String> listProducts() {
+        throw new RuntimeException("Not implemented yet.");
+    }
 }
--- src/main/java/com/github/mgurov/product/ProductService.java
+++ src/main/java/com/github/mgurov/product/ProductService.java
@@ -1,5 +1,16 @@
 package com.github.mgurov.product;
 
+import java.util.List;
+
 public class ProductService {
 
+    private final ProductConnector productConnector;
+
+    public ProductService(ProductConnector productConnector) {
+        this.productConnector = productConnector;
+    }
+
+    public List<String> fetchAwesomeProducts() {
+        return productConnector.listProducts();
+    }
 }
