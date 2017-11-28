bbdae2fa3db46a27830480a272d2c5b77cee16a7
Mykola Gurov <ngurov@gmail.com>
2017-11-28 07:07:26 +0100

    check

---
+++ src/main/java/com/github/mgurov/offer/Offer.java
@@ -0,0 +1,9 @@
+package com.github.mgurov.offer;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Offer {
+}
---
+++ src/main/java/com/github/mgurov/offer/OfferService.java
@@ -0,0 +1,4 @@
+package com.github.mgurov.offer;
+
+public class OfferService {
+}
